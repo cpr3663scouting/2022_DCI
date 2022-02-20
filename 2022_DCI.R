1241e134610d268138012de4b16911bbdb32640c
Chiptang211 <64523069+Chiptang211@users.noreply.github.com>
2022-02-20 15:39:27 -0800

    B12220 - NumArray

--- 2022_DCI.R
+++ 2022_DCI.R
@@ -27,28 +27,28 @@ Dci <- data.frame(Serial=1:nrow(RawData))
 #IF-Event
 
 #IF-MatchNumber
-Dci$MatchNumber <- ifelse(grepl("5", RawData$Stu1),100,0)+
-                   ifelse(!is.na(RawData$Stu26),RawData$Stu26-1,RawData$Stu51+4)*10+
-                   ifelse(!is.na(RawData$Stu27),RawData$Stu27-1,RawData$Stu52+4)
+Dci$'MatchNumber' <- ifelse(grepl("5", RawData$Stu1),100,0)+
+                   ifelse(!is.na(RawData[[26]]),RawData[[26]]-1,RawData[[51]]+4)*10+
+                   ifelse(!is.na(RawData[[27]]),RawData[[27]]-1,RawData[[52]]+4)
 
 #IF-Alliance
-Dci$Alliance <- paste(ifelse(grepl("1", RawData$Stu2), "R", 
-                      ifelse(grepl("2", RawData$Stu2), "B", "")),
+Dci$'Alliance' <- paste(ifelse(grepl("1", RawData[[2]]), "R", 
+                      ifelse(grepl("2", RawData[[2]]), "B", "")),
                       
-                      ifelse(grepl("3", RawData$Stu2), "1", 
-                      ifelse(grepl("4", RawData$Stu2), "2",
-                      ifelse(grepl("5", RawData$Stu2), "3", ""))),
+                      ifelse(grepl("3", RawData[[2]]), "1", 
+                      ifelse(grepl("4", RawData[[2]]), "2",
+                      ifelse(grepl("5", RawData[[2]]), "3", ""))),
                   
                       sep = "")
 
 #IF-ScheduleID
-Dci$ScheduleID <- Dci$MatchNumber*100 + 
-                  ifelse(grepl("13", RawData$Stu2), 1, 
-                  ifelse(grepl("14", RawData$Stu2), 2,
-                  ifelse(grepl("15", RawData$Stu2), 3,
-                  ifelse(grepl("23", RawData$Stu2), 4, 
-                  ifelse(grepl("24", RawData$Stu2), 5,
-                  ifelse(grepl("25", RawData$Stu2), 6, 0))))))
+Dci$'ScheduleID' <- Dci$MatchNumber*100 + 
+                  ifelse(grepl("13", RawData[[2]]), 1, 
+                  ifelse(grepl("14", RawData[[2]]), 2,
+                  ifelse(grepl("15", RawData[[2]]), 3,
+                  ifelse(grepl("23", RawData[[2]]), 4, 
+                  ifelse(grepl("24", RawData[[2]]), 5,
+                  ifelse(grepl("25", RawData[[2]]), 6, 0))))))
 
 #IF-TeamNumber
 
@@ -63,136 +63,236 @@ Dci$ScheduleID <- Dci$MatchNumber*100 +
 #IF-OfficialName
 
 #IF-Rescout
-Dci$"RI-ReScout" <- ifelse(grepl("4", RawData$Stu94), "1", "")
+Dci$'RI-ReScout' <- ifelse(grepl("4", RawData[[94]]), "1", "")
 
 # Auto-Taxi
-Dci$"Auto-Taxi" <- ifelse(grepl("2", RawData$Stu29), "0", 
-                   ifelse(grepl("4", RawData$Stu29), "1", ""))
+Dci$'Auto-Taxi' <- ifelse(grepl("2", RawData[[29]]), "0", 
+                   ifelse(grepl("4", RawData[[29]]), "1", ""))
 
-#Auto-LowScored#
-Dci$"Auto-LowScored#" <- ifelse(!is.na(RawData$Stu7),RawData$Stu7-1,RawData$Stu8+4)
 
-#Auto-LowMissed#
-Dci$"Auto-LowMissed#" <- ifelse(!is.na(RawData$Stu32),RawData$Stu32-1,RawData$Stu33+4)
-
-#Auto-LowAccuracy
-#Auto-LowScore
-
-#Auto-HighScored#
-Dci$"Auto-HighScored#" <- ifelse(!is.na(RawData$Stu57),RawData$Stu57-1,RawData$Stu58+4)
-
-#Auto-HighMissed#
-Dci$"Auto-HighMissed#" <- ifelse(!is.na(RawData$Stu82),RawData$Stu82-1,RawData$Stu83+4)
-
-#Auto-HighAccuracy
-#Auto-HighScore
-#Auto-TotalCargo#
-#Auto-CargoScore
-#Auto-TotalScore
-
+AutoNum <- function(col1, col2) {
+  output <- ifelse(!is.na(RawData[[col1]]),RawData[[col1]]-1,
+            ifelse(!is.na(RawData[[col2]]),RawData[[col2]]+4,""))
+}
 TeleNum <- function(col1, col2, col3, col4) {
   output <- ifelse(!is.na(RawData[[col1]]),RawData[[col1]]-1,
             ifelse(!is.na(RawData[[col2]]),RawData[[col2]]+4,
             ifelse(!is.na(RawData[[col3]]),RawData[[col3]]+9,
             ifelse(!is.na(RawData[[col4]]),RawData[[col4]]+14,""))))
 }
+#Testing
+TestSep <- function(firstCol, numCol) {
+  for(i in firstCol:(firstCol + numCol -1)) {
+    print(RawData[[i]])
+  }
+}
+
+NumArray <- function(firstCol, numCol) {
+  output <- rep(0, nrow(Dci)) 
+  for (i in firstCol:(firstCol + numCol -1)) {
+    output <- output + ifelse(!is.na(RawData[[i]]),RawData[[i]]+5*(i-firstCol)-1,0)
+  }
+  return(output)
+}
+
+
+
+#Auto-LowScored#
+Dci$'Auto-LowScored#' <- NumArray(7,2)
+
+#Auto-LowMissed#
+Dci$'Auto-LowMissed#' <- NumArray(32, 2)
+
+#Auto-LowAccuracy
+Dci$'Auto-LowAccuracy' <- Dci$'Auto-LowScored#'/(Dci$'Auto-LowScored#'+Dci$'Auto-LowMissed#')
+
+#Auto-LowScore
+Dci$''
+
+#Auto-HighScored#
+Dci$'Auto-HighScored#' <- NumArray(57,2)
+
+#Auto-HighMissed#
+Dci$'Auto-HighMissed#' <- NumArray(82,2)
+
+#Auto-HighAccuracy
+Dci$'Auto-HighAccuracy' <- Dci$'Auto-HighScored#'/(Dci$'Auto-HighScored#'+Dci$'Auto-HighMissed#')
+
+#Auto-HighScore
+Dci$''
+
+#Auto-TotalCargo#
+Dci$''
+
+#Auto-CargoScore
+Dci$''
+
+#Auto-TotalScore
+Dci$''
 
 #Tele-LowScored#
-Dci$"Tele-LowScored#" <- TeleNum(13,14,15,16)
+Dci$'Tele-LowScored#' <- NumArray(13,4)
 
 #Tele-LowMissed#
-Dci$"Tele-LowMissed#" <- TeleNum(38,39,40,41)
+Dci$'Tele-LowMissed#' <- NumArray(38,4)
 
 #Tele-LowAccuracy
-
+Dci$'Tele-LowAccuracy' <- Dci$'Tele-LowScored#'/(Dci$'Tele-LowScored#'+Dci$'Tele-LowMissed#')
 
 #Tele-LowScore
-
+Dci$''
 
 #Tele-HighScored#
-Dci$"Tele-HighScored#" <- TeleNum(63,64,65,66)
+Dci$'Tele-HighScored#' <- NumArray(63,4)
 
 #Tele-HighMissed#
-Dci$"#Tele-HighMissed#" <- TeleNum(88,89,90,91)
+Dci$'#Tele-HighMissed#' <- NumArray(88,4)
 
 #Tele-HighAccuracy
-
+Dci$'Tele-HighAccuracy' <- Dci$'Tele-HighScored#'/(Dci$'Tele-HighScored#'+Dci$'Tele-HighMissed#')
 
 #Tele-HighScore
-
+Dci$''
 
 #Tele-TotalCargo#
-
+Dci$''
 
 #Tele-CargoScore
-
+Dci$''
 
 #Tele-TotalScore
-
+Dci$''
 
 #EG-Climb1Attempt
+Dci$''
+
 #EG-Climb1Failed
+Dci$''
+
 #EG-Climb1Latched
+Dci$''
+
 #EG-Climb1Scored
+Dci$''
+
 #EG-Climb1Accuracy
+Dci$''
+
 #EG-Climb1SetUpTime
+Dci$''
+
 #EG-Climb1AscendTime
+Dci$''
 
 #EG-Climb2Attempt
+Dci$''
+
 #EG-Climb2Failed
+Dci$''
+
 #EG-Climb2Latched
+Dci$''
+
 #EG-Climb2Scored
+Dci$''
+
 #EG-Climb2Accuracy
+Dci$''
+
 #EG-Climb2SetUpTime
+Dci$''
+
 #EG-Climb2AscendTime
+Dci$''
+
 
 #EG-Climb3Attempt
+Dci$''
+
 #EG-Climb3Failed
+Dci$''
+
 #EG-Climb3Latched
+Dci$''
+
 #EG-Climb3Scored
+Dci$''
+
 #EG-Climb3Accuracy
+Dci$''
+
 #EG-Climb3SetUpTime
+Dci$''
+
 #EG-Climb3AscendTime
+Dci$''
 
 #EG-Climb4Attempt
+Dci$''
+
 #EG-Climb4Failed
+Dci$''
+
 #EG-Climb4Latched
+Dci$''
+
 #EG-Climb4Scored
+Dci$''
+
 #EG-Climb4Accuracy
+Dci$''
+
 #EG-Climb4SetUpTime
+Dci$''
+
 #EG-Climb4AscendTime
+Dci$''
 
 #EG-TotalScore
+Dci$''
 
-#Total-LowCargo#
+#Total-LowCargo
+Dci$''
+
 #Total-HighCargo#
+Dci$''
+
 #Total-Cargo#
+Dci$''
+
 #Total-LowCargoScore
+Dci$''
+
 #Total-HighCargoScore
+Dci$''
+
 #Total-CargoScore
+Dci$''
+
 #Total-Score
+Dci$''
 
 #RI-Defense
-Dci$"RI-Defense" <- ifelse(grepl("3", RawData$Stu25), "0", 
+Dci$'RI-Defense' <- ifelse(grepl("3", RawData$Stu25), "0", 
                     ifelse(grepl("5", RawData$Stu25), "1", ""))
 
 #RI-Foul
-Dci$"RI-Foul" <- ifelse(grepl("3", RawData$Stu50), "1", "")
+Dci$'RI-Foul' <- ifelse(grepl("3", RawData$Stu50), "1", "")
 
 #RI-Tech+BX1Foul
-Dci$"RI-Tech+BX1Foul" <- ifelse(grepl("5", RawData$Stu50), "1", "")
+Dci$'RI-Tech+BX1Foul' <- ifelse(grepl("5", RawData$Stu50), "1", "")
 
 #RI-NoShow
-Dci$"RI-NoShow" <- ifelse(grepl("3", RawData$Stu75), "1", "")
+Dci$'RI-NoShow' <- ifelse(grepl("3", RawData$Stu75), "1", "")
 
 #RI-Broke
-Dci$"RI-Broke" <- ifelse(grepl("4", RawData$Stu75), "1", "")
+Dci$'RI-Broke' <- ifelse(grepl("4", RawData$Stu75), "1", "")
 
 #RI-Dead
-Dci$"RI-Dead" <- ifelse(grepl("5", RawData$Stu75), "1", "")
+Dci$'RI-Dead' <- ifelse(grepl("5", RawData$Stu75), "1", "")
 
 #RI-Feedback
-Dci$"RI-Feedback" <- ifelse(grepl("5", RawData$Stu94), "1", "")
+Dci$'RI-Feedback' <- ifelse(grepl("5", RawData$Stu94), "1", "")
 
 write.csv(Dci,"TEST.csv",row.names = FALSE)
 
