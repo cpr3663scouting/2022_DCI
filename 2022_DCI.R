115ceb19f493f5867a32ffd2af5ea1c41a5485dd
Chiptang211 <64523069+Chiptang211@users.noreply.github.com>
2022-02-21 20:56:48 -0800

    B12221

--- 2022_DCI.R
+++ 2022_DCI.R
@@ -1,4 +1,13 @@
 library('dplyr')
+library("httr")
+library('jsonlite')
+
+apiKey <- "SBcyOBkFgzIQ8jVIlum24FnnI4KPq4VSA5MYtdCVMgDrZYaMfYduTjMGKO5A9AVz"
+result <- GET("https://frc-api.firstinspires.org/v3.0/",add_headers("X-TBA-Auth-Key" = apiKey))
+
+team <- GET("https://www.thebluealliance.com/api/v3/events/2022",add_headers("X-TBA-Auth-Key" = apiKey))
+
+
 
 #Import ZipGrade Data
 RawZipGrade <- read.csv('ExportQuizFullDetail-S12Testing.csv')
@@ -16,59 +25,20 @@ RawData <- select(RawZipGrade, Stu1, Stu2, Stu3, Stu4, Stu5, Stu6, Stu7, Stu8, S
                   Stu100)
 
 #ScouterID Information
-Id <- c("A1A","A1B","A1C","A1D","A1E","A2A","A2B","A2C","A2D","A2E")
-Name <- c("MaxLY","AvaH","KathleenK","NatalieS","NoahR","TakeshiT","IsaacK","MaliaL","GabeM","ChipT")
-IssId <- data.frame(Id,Name)
-
+IssId <- read.csv('ISS_ID.csv')
+IssId$'IF-ScouterSeq' <- mutate(IssId, IdSeq)
 
 #Data Collection Interface
 Dci <- data.frame(Serial=1:nrow(RawData))
 
-#IF-Event
-
-#IF-MatchNumber
-Dci$'MatchNumber' <- ifelse(grepl("5", RawData[[1]]),100,0)+
-                   ifelse(!is.na(RawData[[26]]),RawData[[26]]-1,RawData[[51]]+4)*10+
-                   ifelse(!is.na(RawData[[27]]),RawData[[27]]-1,RawData[[52]]+4)
-
-#IF-Alliance
-Dci$'Alliance' <- paste(ifelse(grepl("1", RawData[[2]]), "R", 
-                      ifelse(grepl("2", RawData[[2]]), "B", "")),
-                      
-                      ifelse(grepl("3", RawData[[2]]), "1", 
-                      ifelse(grepl("4", RawData[[2]]), "2",
-                      ifelse(grepl("5", RawData[[2]]), "3", ""))),
-                  
-                      sep = "")
-
-#IF-ScheduleID
-Dci$'ScheduleID' <- Dci$MatchNumber*100 + 
-                  ifelse(grepl("13", RawData[[2]]), 1, 
-                  ifelse(grepl("14", RawData[[2]]), 2,
-                  ifelse(grepl("15", RawData[[2]]), 3,
-                  ifelse(grepl("23", RawData[[2]]), 4, 
-                  ifelse(grepl("24", RawData[[2]]), 5,
-                  ifelse(grepl("25", RawData[[2]]), 6, 0))))))
-
-#IF-TeamNumber
-
-#IF-TeamName
-
-#IF-ScouterID
-
-#IF-ScouterName
-
-#IF-OfficialID
-
-#IF-OfficialName
-
-#IF-Rescout
-Dci$'RI-ReScout' <- ifelse(grepl("4", RawData[[94]]), 1, 0)
-
-
 #Functions
-CargoAccuracy <- function(scored, missed) {
-  ifelse(scored + missed == 0, rep("", nrow(Dci)), scored/(scored+missed))
+AsciiToChar <- function(col) {
+  output <- c()
+  for (i in 1:nrow(Dci)) {
+    output <- append(output,intToUtf8(RawData[i,col]+64))
+  }
+  output[is.na(output)] <- ""
+  return(output)
 }
 
 NumArray <- function(firstCol, numCol) {
@@ -79,8 +49,20 @@ NumArray <- function(firstCol, numCol) {
   return(output)
 }
 
+CargoAccuracy <- function(scored, missed) {
+  ifelse(scored + missed == 0, rep(NA, nrow(Dci)), scored/(scored+missed))
+}
+
 Point <- function(item, point) {
-  
+  ifelse(is.na(item), rep(0,nrow(Dci)), item*point)
+}
+
+Bubble <- function(col, index) {
+  ifelse(grepl(index, RawData[[col]]), 1, 0)
+}
+
+ClimbAccuracy <- function(attempted, latched, scored) {
+  ifelse(attempted == 0, rep(NA, nrow(Dci)), (latched + scored)/attempted)
 }
 
 #Backups
@@ -101,13 +83,63 @@ TestSep <- function(firstCol, numCol) {
 }
 
 
+#IF-Event
+
+#IF-MatchNumber
+Dci$'MatchNumber' <- ifelse(grepl("5", RawData[[1]]),100,0)+
+  ifelse(!is.na(RawData[[26]]),RawData[[26]]-1,RawData[[51]]+4)*10+
+  ifelse(!is.na(RawData[[27]]),RawData[[27]]-1,RawData[[52]]+4)
+
+#IF-Alliance
+Dci$'Alliance' <- paste(ifelse(grepl("1", RawData[[2]]), "R", 
+                        ifelse(grepl("2", RawData[[2]]), "B", "")),
+                        
+                        ifelse(grepl("3", RawData[[2]]), "1", 
+                        ifelse(grepl("4", RawData[[2]]), "2",
+                        ifelse(grepl("5", RawData[[2]]), "3", ""))),
+                        
+                        sep = "")
+
+#IF-ScheduleID
+Dci$'ScheduleID' <- Dci$MatchNumber*100 + 
+  ifelse(grepl("13", RawData[[2]]), 1, 
+  ifelse(grepl("14", RawData[[2]]), 2,
+  ifelse(grepl("15", RawData[[2]]), 3,
+  ifelse(grepl("23", RawData[[2]]), 4, 
+  ifelse(grepl("24", RawData[[2]]), 5,
+  ifelse(grepl("25", RawData[[2]]), 6, 0))))))
+
+
+#IF-TeamNumber
+
+#IF-TeamName
+
+#IF-ScouterID
+Dci$'IF-ScouterID' <- paste(AsciiToChar(3), ifelse(is.na(RawData[[28]]),"",RawData[[28]]), AsciiToChar(53), sep = "")
+
+#IF-ScouterSeq
+Dci$'IF-ScouterSeq' <- ifelse(is.na(RawData[[3]]),0,RawData[[3]]-1)*25 +
+                       ifelse(is.na(RawData[[28]]),0,RawData[[28]]-1)*5 +
+                       ifelse(is.na(RawData[[53]]),0,RawData[[53]])
+
+#IF-ScouterName
+Dci$'IF-ScouterName'
+
+#IF-OfficialID
+Dci$'IF-OfficialID' <- paste(AsciiToChar(76), ifelse(is.na(RawData[[77]]),"",RawData[[77]]), AsciiToChar(78), sep = "")
+
+#IF-OfficialName
+
+
+#IF-Rescout
+Dci$'RI-ReScout' <- Bubble(94, 4)
 
 # Auto-Taxi
 Dci$'Auto-Taxi' <- ifelse(grepl("2", RawData[[29]]), 0, 
                    ifelse(grepl("4", RawData[[29]]), 1, NA))
 
-# Auto-TaxiScore
-Dci$'Auto-TaxiScore' <- ifelse(is.na(Dci$'Auto-Taxi'), rep(0,nrow(Dci)), Dci$'Auto-Taxi'*2)
+# Auto-TaxiPoint
+Dci$'Auto-TaxiPoint' <- Point(Dci$'Auto-Taxi',2)
 
 #Auto-LowScored#
 Dci$'Auto-LowScored#' <- NumArray(7,2)
@@ -116,10 +148,10 @@ Dci$'Auto-LowScored#' <- NumArray(7,2)
 Dci$'Auto-LowMissed#' <- NumArray(32, 2)
 
 #Auto-LowAccuracy
-Dci$'Auto-LowAccuracy' <- Dci$'Auto-LowScored#'/(Dci$'Auto-LowScored#'+ Dci$'Auto-LowMissed#')
+Dci$'Auto-LowAccuracy' <- CargoAccuracy(Dci$'Auto-LowScored#',Dci$'Auto-LowMissed#')
 
-#Auto-LowScore
-Dci$''
+#Auto-LowPoint
+Dci$'Auto-LowPoint' <- Point(Dci$'Auto-LowScored#',2)
 
 #Auto-HighScored#
 Dci$'Auto-HighScored#' <- NumArray(57,2)
@@ -128,19 +160,19 @@ Dci$'Auto-HighScored#' <- NumArray(57,2)
 Dci$'Auto-HighMissed#' <- NumArray(82,2)
 
 #Auto-HighAccuracy
-Dci$'Auto-HighAccuracy' <- Dci$'Auto-HighScored#'/(Dci$'Auto-HighScored#' + Dci$'Auto-HighMissed#')
+Dci$'Auto-HighAccuracy' <- CargoAccuracy(Dci$'Auto-HighScored#',Dci$'Auto-HighMissed#')
 
-#Auto-HighScore
-Dci$''
+#Auto-HighPoint
+Dci$'Auto-HighPoint' <- Point(Dci$'Auto-HighScored#',4)
 
 #Auto-TotalCargo#
-Dci$''
+Dci$'Auto-TotalCargo#' <- Dci$'Auto-LowScored#' + Dci$'Auto-HighScored#'
 
-#Auto-CargoScore
-Dci$''
+#Auto-CargoPoint
+Dci$'Auto-CargoPoint' <- Dci$'Auto-LowPoint' + Dci$'Auto-HighPoint'
 
-#Auto-TotalScore
-Dci$''
+#Auto-TotalPoint
+Dci$'Auto-TotalPoint' <- Dci$'Auto-TaxiPoint' + Dci$'Auto-LowPoint' + Dci$'Auto-HighPoint'
 
 #Tele-LowScored#
 Dci$'Tele-LowScored#' <- NumArray(13,4)
@@ -149,10 +181,10 @@ Dci$'Tele-LowScored#' <- NumArray(13,4)
 Dci$'Tele-LowMissed#' <- NumArray(38,4)
 
 #Tele-LowAccuracy
-Dci$'Tele-LowAccuracy' <- Dci$'Tele-LowScored#'/(Dci$'Tele-LowScored#' + Dci$'Tele-LowMissed#')
+Dci$'Tele-LowAccuracy' <- CargoAccuracy(Dci$'Tele-LowScored#',Dci$'Tele-LowMissed#')
 
 #Tele-LowScore
-Dci$''
+Dci$'Tele-LowPoint' <- Point(Dci$'Tele-LowScored#', 1)
 
 #Tele-HighScored#
 Dci$'Tele-HighScored#' <- NumArray(63,4)
@@ -160,172 +192,131 @@ Dci$'Tele-HighScored#' <- NumArray(63,4)
 #Tele-HighMissed#
 Dci$'#Tele-HighMissed#' <- NumArray(88,4)
 
-
-
-Dci$'Auto-LowScored#'==0
-
-
-
-print(Dci$'Auto-LowScored#'==0)
-
-
-
-
-
-
-
-
-
-
-
-
-
-print(NumArray(63,4))
-print(NumArray(88,4))
-
-#Tele-HighScore
-Dci$''
+#Tele-HighPoint
+Dci$'Tele-HighPoint' <- Point(Dci$'Tele-HighScored#', 2)
 
 #Tele-TotalCargo#
-Dci$''
+Dci$'Tele-TotalCargo#' <- Dci$'Tele-LowScored#' + Dci$'Tele-HighScored#'
 
 #Tele-CargoScore
-Dci$''
+Dci$'Tele-CargoScore' <- Dci$'Tele-LowPoint' + Dci$'Tele-HighPoint'
 
 #Tele-TotalScore
-Dci$''
+Dci$'Tele-TotalScore' <- Dci$'Tele-LowPoint' + Dci$'Tele-HighPoint'
 
 #EG-Climb1Attempt
-Dci$''
+Dci$'EG-Climb1Attempt' <- Bubble(19, "3") + Bubble(19, "4") + Bubble(19, "5")
 
 #EG-Climb1Failed
-Dci$''
+Dci$'EG-Climb1Failed' <- Bubble(19, "3")
 
 #EG-Climb1Latched
-Dci$''
+Dci$'EG-Climb1Latched' <- Bubble(19, "4")
 
 #EG-Climb1Scored
-Dci$''
+Dci$'EG-Climb1Scored' <- Bubble(19, "5")
 
 #EG-Climb1Accuracy
-Dci$''
-
-#EG-Climb1SetUpTime
-Dci$''
-
-#EG-Climb1AscendTime
-Dci$''
+Dci$'EG-Climb1Accuracy' <- ClimbAccuracy(Dci$'EG-Climb1Attempt', Dci$'EG-Climb1Latched', Dci$'EG-Climb1Scored')
 
 #EG-Climb2Attempt
-Dci$''
+Dci$'EG-Climb2Attempt' <- Bubble(20, "3") + Bubble(20, "4") + Bubble(20, "5")
 
 #EG-Climb2Failed
-Dci$''
+Dci$'EG-Climb2Failed' <- Bubble(20, "3")
 
 #EG-Climb2Latched
-Dci$''
+Dci$'EG-Climb2Latched' <- Bubble(20, "4")
 
 #EG-Climb2Scored
-Dci$''
+Dci$'EG-Climb2Scored' <- Bubble(20, "5")
 
 #EG-Climb2Accuracy
-Dci$''
-
-#EG-Climb2SetUpTime
-Dci$''
-
-#EG-Climb2AscendTime
-Dci$''
-
+Dci$'EG-Climb2Accuracy' <- ClimbAccuracy(Dci$'EG-Climb2Attempt', Dci$'EG-Climb2Latched', Dci$'EG-Climb2Scored')
 
 #EG-Climb3Attempt
-Dci$''
+Dci$'EG-Climb3Attempt' <- Bubble(21, "3") + Bubble(21, "4") + Bubble(21, "5")
 
 #EG-Climb3Failed
-Dci$''
+Dci$'EG-Climb3Failed' <- Bubble(21, "3")
 
 #EG-Climb3Latched
-Dci$''
+Dci$'EG-Climb3Latched' <- Bubble(21, "4")
 
 #EG-Climb3Scored
-Dci$''
+Dci$'EG-Climb3Scored' <- Bubble(21, "5")
 
 #EG-Climb3Accuracy
-Dci$''
-
-#EG-Climb3SetUpTime
-Dci$''
-
-#EG-Climb3AscendTime
-Dci$''
+Dci$'EG-Climb3Accuracy' <- ClimbAccuracy(Dci$'EG-Climb3Attempt', Dci$'EG-Climb3Latched', Dci$'EG-Climb3Scored')
 
 #EG-Climb4Attempt
-Dci$''
+Dci$'EG-Climb4Attempt' <- Bubble(21, "3") + Bubble(21, "4") + Bubble(21, "5")
 
 #EG-Climb4Failed
-Dci$''
+Dci$'EG-Climb4Failed' <- Bubble(22, "3")
 
 #EG-Climb4Latched
-Dci$''
+Dci$'EG-Climb4Latched' <- Bubble(22, "4")
 
 #EG-Climb4Scored
-Dci$''
+Dci$'EG-Climb4Scored' <- Bubble(22, "5")
 
 #EG-Climb4Accuracy
-Dci$''
+Dci$'EG-Climb4Accuracy' <- ClimbAccuracy(Dci$'EG-Climb4Attempt', Dci$'EG-Climb4Latched', Dci$'EG-Climb4Scored')
 
-#EG-Climb4SetUpTime
-Dci$''
+#EG-ClimbSetUpTime
+Dci$'EG-ClimbSetUpTime' <- ifelse(grepl(1, RawData[[44]]), NA, NumArray(44,4))
 
-#EG-Climb4AscendTime
-Dci$''
+#EG-ClimbAscendTime
+Dci$'EG-ClimbAscendTime' <- ifelse(grepl(1, RawData[[69]]), NA, NumArray(69,4))
 
 #EG-TotalScore
-Dci$''
+Dci$'EG-TotalScore' <- Dci$'EG-Climb1Scored' + Dci$'EG-Climb2Scored' + Dci$'EG-Climb3Scored' + Dci$'EG-Climb4Scored'
 
 #Total-LowCargo
-Dci$''
+Dci$'Total-LowCargo'
 
 #Total-HighCargo#
-Dci$''
+Dci$'Total-HighCargo#'
 
 #Total-Cargo#
-Dci$''
+Dci$'Total-Cargo#'
 
 #Total-LowCargoScore
-Dci$''
+Dci$'Total-LowCargoScore'
 
 #Total-HighCargoScore
-Dci$''
+Dci$'Total-HighCargoScore'
 
 #Total-CargoScore
-Dci$''
+Dci$'Total-CargoScore'
 
 #Total-Score
-Dci$''
+Dci$'Total-Score'
 
 #RI-Defense
-Dci$'RI-Defense' <- ifelse(grepl("3", RawData$Stu25), "0", 
-                    ifelse(grepl("5", RawData$Stu25), "1", ""))
+Dci$'RI-Defense' <- ifelse(grepl("3", RawData[[25]]), 0, 
+                    ifelse(grepl("5", RawData[[25]]), 1, NA))
 
 #RI-Foul
-Dci$'RI-Foul' <- ifelse(grepl("3", RawData$Stu50), "1", "")
+Dci$'RI-Foul' <- Bubble(50, 3)
 
 #RI-Tech+BX1Foul
-Dci$'RI-Tech+BX1Foul' <- ifelse(grepl("5", RawData$Stu50), "1", "")
+Dci$'RI-Tech+BX1Foul' <- Bubble(50, 5)
 
 #RI-NoShow
-Dci$'RI-NoShow' <- ifelse(grepl("3", RawData$Stu75), "1", "")
+Dci$'RI-NoShow' <- Bubble(75, 3)
 
 #RI-Broke
-Dci$'RI-Broke' <- ifelse(grepl("4", RawData$Stu75), "1", "")
+Dci$'RI-Broke' <- Bubble(75, 4)
 
 #RI-Dead
-Dci$'RI-Dead' <- ifelse(grepl("5", RawData$Stu75), "1", "")
+Dci$'RI-Dead' <- Bubble(75, 5)
 
 #RI-Feedback
-Dci$'RI-Feedback' <- ifelse(grepl("5", RawData$Stu94), "1", "")
+Dci$'RI-Feedback' <- Bubble(75, 5)
 
+#Output
 write.csv(Dci,"TEST.csv",row.names = FALSE)
 
 
